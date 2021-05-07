73ea31ecaecd35fd17b99fa9c5da94e8ffaaf711
Vandit Jain <mailvandit.j@gmail.com>
2021-05-07 18:04:27 +0530

    minor changes in console text color

--- getAppointmentsCovin.js
+++ getAppointmentsCovin.js
@@ -61,7 +61,7 @@ function printTime(){
 }
 
 function printNoAppointments(){
-    console.log("No Appointments Yet")
+    console.log('\x1b[31m%s\x1b[0m',"No Appointments Yet")
 }
 
 Date.prototype.today = function () { 
